610ee29dacc6ae187d3b17182db4608c742b667a
José Carlos Pérez Vilchis <josecarlosperezvilchis@graphic-design26.local>
2015-11-26 15:02:10 -0600

    Initial Commit

---
+++ TareaVel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TareaVel
+//
+//  Created by José Carlos Pérez Vilchis on 26/11/15.
+//  Copyright © 2015 José Carlos Pérez Vilchis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
